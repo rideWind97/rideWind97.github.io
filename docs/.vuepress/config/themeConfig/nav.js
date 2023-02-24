368e73d2b8c3e97f1388a5b7ff379d7a6e9bf5e3
rideWind97 <47934820+rideWind97@users.noreply.github.com>
2023-02-24 18:04:15 +0800

    Deploying to gh-pages from @ rideWind97/rideWind97.github.io@31f192f5e03914ca4a75ba67b6cfcc6990ee65a2 🚀

--- docs/.vuepress/config/themeConfig/nav.js
+++
@@ -1,9 +0,0 @@
-// nav
-module.exports = [
-  { text: '首页', link: '/' },
-  { text: 'one',link: '/one/',},
-  { text: 'two',link: '/two/',},
-  { text: 'three', link: '/three/' },
-  { text: '留言板', link: '/message-board/' },
-  { text: '我的博客', link: 'https://github.com/rideWind97/chengfeng-blog' },
-]
